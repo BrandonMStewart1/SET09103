29a0f0027a7f96a6874cca7d89eb80d4cb4c3db5
BrandonMStewart1 <40582340@live.napier.ac.uk>
2023-11-16 12:54:02 +0000

    gaming

--- myapp.py
+++ myapp.py
@@ -6,23 +6,24 @@ basket = {}
 @app.route('/')
 def root():
     total_price = sum([2.0 for _ in basket])
-    return render_template('Home.html', basket=basket)
+    return render_template('Home.html', basket=basket,)
 
-@app.route('/add_to_basket/<item>', methods=['POST'])
-def add_to_basket(item):
-    if item in basket:
-        basket[item] += 1
-    else:
-        basket[item] = 1
-    return redirect('/shop')
+@app.route('/add_to_basket/<item>/<float:price>', methods=['POST'])
+def add_to_basket(item,price):
+        basket.append((item,2.0))
+        return render_template('shop.html' basket=basket)
+            
+
+        total_price = sum([item_dict['price'] * item_dict['quantity'] for item_dict in basket])
+        return render_template('Home.html', basket=basket, total_price=total_price)
 
 @app.route('/remove_from_basket/<item>', methods=['POST'], endpoint='remove_from_basket')
 def remove_from_basket(item):
-            if item in basket:
-                basket[item] -= 1
-                if basket[item] == 0:
-                    del basket[item]
-                return redirect('/shop')
+    for i, (basket_item,_) in enumerate(basket):
+        if basket_item == item:
+            del basket[i]
+            break
+    return render_template('shop.html', basket=basket or [])
 
 @app.route('/caves', endpoint='caves')
 def caves():
